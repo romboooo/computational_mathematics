6075c7601d0869daff8cee5b920b2952fb7e7b25
Роман Бурейко <144876262+romboooo@users.noreply.github.com>
2025-06-16 15:47:57 +0300

    feat: fucntion input

--- lab5/main.py
+++ lab5/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import math 
 from methods.newtonWithFiniteDif import newtonWithFiniteDifferences
 from methods.newtonWithDivDif import newtonWithDividedDifferences
 from methods.lagrange import lagrange
@@ -7,18 +7,59 @@ import time
 
 def readData():
     print("каким образом вводим данные?")
+    print("(file/console/function)")
     while True:
-        print("ответ принимается в формате (f/c)")
+        print("ответ принимается в формате (fi/c/fu)")
         inp = input()   
         inp = inp.lower()
-        if inp == "c" or inp == "f":
+        if inp == "c" or inp == "fi" or inp == "fu":
             break
         print("неверный ввод")
 
     if inp == "c":
         return readFromConsole()
-    return readFromFile()
+    elif inp == "fi":
+        return readFromFile()
+    else:
+        return readFromFunction()
 
+def readFromFunction():
+    func_arr = {1: func1, 2: func2}
+
+    print("Выберите функцию из предложенных:")
+    print("1) exp(x)")
+    print("2) x + 2x^2")
+    f_numb = input()
+    try:
+        f_numb = int(f_numb)
+    except: 
+        print("ожидалось число")
+        readData()
+        return
+    if f_numb != 1 and f_numb !=2 : 
+        print("ожидалось числа 1 или 2")
+        readData()
+        return
+    func = func_arr.get(f_numb)
+    print(f_numb)
+    a = float(input("Введите левую границу интервала: "))
+    b = float(input("Введите правую границу интервала: "))
+    n = int(input("Введите количество точек на интервале: "))
+    x_ = float(input("Введите значение аргумента: "))
+    h = (b - a) / (n - 1)
+    x = []
+    y = []
+
+    for i in range(0, n):
+        x.append(a + i * h)
+        y.append(func(x[i]))
+    return np.array(x),np.array(y),x_
+
+def func1(x):
+    return math.exp(x)
+
+def func2(x):
+    return x + 2 * x ** 2
 
 def readFromConsole():
     print("Введите значения x через пробел:")
@@ -43,8 +84,6 @@ def dispatchToConsole():
     print("программа завершается")
     time.sleep(2)
     exit()
-
-import numpy as np
 
 def readFromFile():
     try:
@@ -86,13 +125,32 @@ def readFromFile():
         return dispatchToConsole()
     return x, y, x_
 
-
+def isDividedDifferences(X) -> bool:
+    if len(X) < 2:
+        print("Для проверки нужно как минимум 2 узла")
+        return False
+    
+    base_step = X[1] - X[0]
+    
+    for i in range(1, len(X) - 1):
+        current_step = X[i + 1] - X[i]
+        if not (abs(current_step - base_step) < 1e-6 * max(1.0, abs(base_step))):
+            print(f"узлы не равноотстоящие")
+            return False
+    
+    print(f"Узлы равноотстоящие с шагом {base_step:.2f}")
+    return True
 
 def main():
     x,y,x_ = readData()
-    lagrange(x,y,x_)
-    newtonWithDividedDifferences(x,y,x_)
-    newtonWithFiniteDifferences(x,y,x_)
+    newtonOut = 0
+    if isDividedDifferences(x):
+        newtonOut = newtonWithFiniteDifferences(x,y,x_)
+    else:
+        newtonOut = newtonWithDividedDifferences(x,y,x_)
+
+    newtonOut = lagrange(x,y,x_)
+
 
 if __name__ == "__main__":
     main()
