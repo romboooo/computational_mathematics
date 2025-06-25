8660e46b61cfd6bd9f1d9fcbf0a2208ee9db77ee
huji <389491@niuitmo.ru>
2025-06-25 16:27:57 +0300

    chore: format lab5,6

--- lab6/data_reader.py
+++ lab6/data_reader.py
@@ -1,7 +1,9 @@
 import math
 
+
 def choose_odu():
     import math
+
     print("Выберите ОДУ:")
     print("1. y' = x + y")
     print("2. y' = sin(x) - y")
@@ -9,23 +11,29 @@ def choose_odu():
 
     while True:
         try:
-            choice = int(input('> Выберите ОДУ [1/2/3]: '))
+            choice = int(input("> Выберите ОДУ [1/2/3]: "))
             if choice == 1:
                 f = lambda x, y: x + y
                 exact_y = lambda x, x0, y0: math.exp(x - x0) * (y0 + x0 + 1) - x - 1
                 return f, exact_y
             elif choice == 2:
                 f = lambda x, y: math.sin(x) - y
-                exact_y = lambda x, x0, y0: (y0 - 0.5*math.sin(x0) + 0.5*math.cos(x0)) * math.exp(x0 - x) + 0.5*math.sin(x) - 0.5*math.cos(x)
+                exact_y = (
+                    lambda x, x0, y0: (y0 - 0.5 * math.sin(x0) + 0.5 * math.cos(x0))
+                    * math.exp(x0 - x)
+                    + 0.5 * math.sin(x)
+                    - 0.5 * math.cos(x)
+                )
                 return f, exact_y
             elif choice == 3:
                 f = lambda x, y: y / x
-                exact_y = lambda x, x0, y0: y0 * math.exp(math.log(x) - math.log(x0))  
+                exact_y = lambda x, x0, y0: y0 * math.exp(math.log(x) - math.log(x0))
                 return f, exact_y
             else:
                 print("Некорректный ввод! Попробуйте снова")
         except ValueError:
             print("Некорректный ввод! Введите число")
+
 
 def read_data():
     while True:
